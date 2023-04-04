c59a9e6a9696765c66b62c587a0df263d1c97aae
abidkyo <kyotasuna95@gmail.com>
2023-04-04 16:02:49 +0200

    ENH sh linter

--- lvim/after/ftplugin/sh.lua
+++ lvim/after/ftplugin/sh.lua
@@ -1,2 +1,5 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { command = "shfmt", filetypes = { "sh" } } }
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "shellcheck", filetypes = { "sh" } } }
