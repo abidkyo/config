bd0738525242cb432d600f4253602a5c02eba385
abidkyo <kyotasuna95@gmail.com>
2023-05-06 00:43:28 +0200

    ENH shfmt tab two spaces

--- lvim/after/ftplugin/sh.lua
+++ lvim/after/ftplugin/sh.lua
@@ -1,5 +1,5 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup { { command = "shfmt", filetypes = { "sh" } } }
+formatters.setup { { command = "shfmt", args = {"-i", "2"}, filetypes = { "sh" } } }
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { { command = "shellcheck", filetypes = { "sh" } } }
