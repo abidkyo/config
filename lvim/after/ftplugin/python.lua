32faa433d0fb10b7829449a4616e343ccb7031ce
abidkyo <kyotasuna95@gmail.com>
2024-01-27 17:55:04 +0100

    ENH update lvim config
    black and flake8 options

--- lvim/after/ftplugin/python.lua
+++ lvim/after/ftplugin/python.lua
@@ -1,5 +1,6 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup { { command = "black", args = { "--line-length", "120" }, filetypes = { "python" } } }
+formatters.setup { { command = "black", filetypes = { "python" } } }
 
+-- 88 max line length according to black default
 local linters = require "lvim.lsp.null-ls.linters"
-linters.setup { { command = "flake8", args = { "--max-line-length", "120" }, filetypes = { "python" } } }
+linters.setup { { command = "flake8", args = { "--max-line-length=88", "--extend-ignore=E203" }, filetypes = { "python" } } }
