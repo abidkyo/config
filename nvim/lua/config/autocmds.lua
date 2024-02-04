6ec9547c70adb48656acca1603f745ec89ce58ab
abidkyo <kyotasuna95@gmail.com>
2024-02-04 11:52:43 +0100

    ENH nvim close file with q
    ... for some filetypes

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -8,3 +8,21 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.opt.tabstop = 4
   end,
 })
+
+-- q keymap to close file
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {
+    "PlenaryTestPopup",
+    "checkhealth",
+    "help",
+    "lspinfo",
+    "man",
+    "netrw",
+    "notify",
+    "qf",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
