f1fae21645d3ba392ae80dbbf90fb8c90d41cc5a
abidkyo <kyotasuna95@gmail.com>
2024-02-13 21:25:47 +0100

    ENH nvim cmp for autocompletion

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,6 +1,7 @@
 -- editor configs
 -- neo-tree
 -- comment
+-- nvim-cmp
 
 return {
   {
@@ -33,5 +34,48 @@ return {
         extra = false,
       },
     },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    version = false,
+    event = { "InsertEnter" },
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- cmp source buffer
+      "hrsh7th/cmp-path", -- cmp source paths
+      "L3MON4D3/LuaSnip", -- snippet engine
+      "saadparwaiz1/cmp_luasnip", -- snippet autocompletion
+      "rafamadriz/friendly-snippets", -- vscode snippets
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      -- load vscode style snippets (friendly-snippets)
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        }),
+        sources = cmp.config.sources({
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
   },
 }
