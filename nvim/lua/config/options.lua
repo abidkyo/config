9897cf8530c8b8ec3249658ed22769ccc6f71045
abidkyo <kyotasuna95@gmail.com>
2024-02-24 10:07:30 +0100

    ENH diagnostic config and icon

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -44,4 +44,16 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- diagnostic
-vim.diagnostic.config({ virtual_text = false })
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
+local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
