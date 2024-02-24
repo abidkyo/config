b532e75c67ffb8a0557ee88d5a8bcbb92ed20ff4
abidkyo <kyotasuna95@gmail.com>
2024-02-24 09:30:50 +0100

    MNT linter setup
    - disable linter for sh file, in favour of bashls

--- nvim/lua/plugins/linter.lua
+++ nvim/lua/plugins/linter.lua
@@ -3,15 +3,14 @@
 
 return {
   "mfussenegger/nvim-lint",
-  dependencies = "williamboman/mason.nvim", -- neeeded to find formatter binary
+  dependencies = "williamboman/mason.nvim", -- neeeded to find linter binary
   event = "BufRead",
+  ft = { "py" },
   config = function()
     local lint = require("lint")
 
     lint.linters_by_ft = {
       python = { "pylint" },
-      bash = { "shellcheck" },
-      sh = { "shellcheck" },
     }
 
     vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost" }, {
